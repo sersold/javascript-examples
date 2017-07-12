a802ee61a67a0548e8d606f7686a94bf1eac0149
Sergey Soldatenkov <soldatenkovse@gmail.com>
2017-07-12 15:29:38 +0300

    Added SDE

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'index.bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+  },
+
+  module: {
+  rules: [
+    {
+      test: /\.js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['env']
+        }
+      }
+    }
+  ]
+}
+};
